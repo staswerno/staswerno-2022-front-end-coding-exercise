f7fd6a58b38fdaf1a78f189bf8ec4029cb11fe87
Staś Werno <89650713+staswerno@users.noreply.github.com>
2022-04-08 10:17:14 +0200

    add data

---
+++ src/data.js
@@ -0,0 +1,50 @@
+import chopandswing from "./images/chop-and-swing.jpg";
+import corelibrary from "./images/core-library.jpg";
+import drumbooth from "./images/drum-booth.jpg";
+import drumessentials from "./images/drum-essentials.jpg";
+import electrickeyboards from "./images/electric-keyboards.jpg";
+import glitchandwash from "./images/glitch-and-wash.jpg";
+import punchandtilt from "./images/punch-and-tilt.jpg";
+import skitterandstep from "./images/skitter-and-step.jpg";
+import synthessentials from "./images/synth-essentials.jpg";
+
+const soundCategories = [
+	{
+		name: "Drum Essentials",
+		image: drumessentials,
+	},
+	{
+		name: "Glitch and Wash",
+		image: glitchandwash,
+	},
+	{
+		name: "Synth Essentials",
+		image: synthessentials,
+	},
+	{
+		name: "Drum Booth",
+		image: drumbooth,
+	},
+	{
+		name: "Punch and Tilt",
+		image: punchandtilt,
+	},
+	{
+		name: "Electric Keyboards",
+		image: electrickeyboards,
+	},
+	{
+		name: "Skitter and Step",
+		image: skitterandstep,
+	},
+	{
+		name: "Chop and Swing",
+		image: chopandswing,
+	},
+	{
+		name: "Core Library",
+		image: corelibrary,
+	},
+];
+
+export { soundCategories };
