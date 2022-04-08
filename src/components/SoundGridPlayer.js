2fec5cf325a215fe0afc36ede7cdc8dca262f56b
Staś Werno <89650713+staswerno@users.noreply.github.com>
2022-04-08 12:06:41 +0200

    add basic play/pause button functionality

--- src/components/SoundGridPlayer.js
+++ src/components/SoundGridPlayer.js
@@ -1,12 +1,27 @@
 import SoundGridPlayerItem from "./SoundGridPlayerItem";
 import { soundCategories as categories } from "../data";
+import { useState } from "react";
 
 export default function SoundGridPlayer() {
+	const [selectedSound, setSelectedSound] = useState(null);
 	return (
 		<div className="grid grid-cols-3 max-w-5xl">
-			{categories.map((element, index) => (
-				<SoundGridPlayerItem key={index} element={element} index={index} />
-			))}
+			{categories.map((element, index) => {
+				const playHandler = () => {
+					selectedSound !== index
+						? setSelectedSound(index)
+						: setSelectedSound(null);
+				};
+				return (
+					<SoundGridPlayerItem
+						key={index}
+						element={element}
+						index={index}
+						playHandler={playHandler}
+						selectedSound={selectedSound}
+					/>
+				);
+			})}
 		</div>
 	);
 }
--- src/components/SoundGridPlayerItem.js
+++ src/components/SoundGridPlayerItem.js
@@ -1,7 +1,12 @@
 import playicon from "../icons/play-icon.svg";
 import pauseicon from "../icons/pause-icon.svg";
 
-export default function SoundGridPlayerItem({ element }) {
+export default function SoundGridPlayerItem({
+	element,
+	index,
+	playHandler,
+	selectedSound,
+}) {
 	return (
 		<div
 			className="aspect-square w-full flex relative"
@@ -13,8 +18,12 @@ export default function SoundGridPlayerItem({ element }) {
 			<div className="hidden sm:block absolute bottom-0 w-full h-1/3 bg-gradient-to-t from-black"></div>
 			<div className="self-end flex relative">
 				<div className="h-[36px] w-[36px] bg-ableton-purple flex">
-					<button className="m-auto">
-						<img src={playicon} alt="audio control icon" />
+					<button className="m-auto" onClick={playHandler}>
+						{selectedSound === index ? (
+							<img src={pauseicon} alt="pause button" />
+						) : (
+							<img src={playicon} alt="play button" />
+						)}
 					</button>
 				</div>
 				<div className="hidden sm:block ml-3 font-book text-xs self-center text-white">
